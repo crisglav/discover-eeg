2f304fc76d5ec1478af8201cf957dd1fa24bdc79
Cristina Gil <cristina.gil@tum.de>
2022-08-05 17:10:31 +0200

    use ft_virtualchannels in dwpli_felix

--- custom_functions/compute_dwpli_felix.m
+++ custom_functions/compute_dwpli_felix.m
@@ -6,25 +6,22 @@ function compute_dwpli_felix(params,bidsID,freqBand)
 % Load EEG data
 data = load_preprocessed_data(params,bidsID);
 
+% Band-pass filter the data in the relevant frequency band
+cfg = [];
+cfg.bpfilter = 'yes';
+cfg.bpfreq = params.freq_band.(freqBand);
+data = ft_preprocessing(cfg, data);
+
 % Load source reconstruction data
 load(fullfile(params.source_folder,[bidsID '_source_' freqBand '.mat']),'source');
+nVoxel = sum(source.inside);
 
 % Reconstruct the virtual time series (apply spatial filter to sensor level
 % data)
-nElec =  numel(data.label);
-nVoxel = length([source.avg.filter{:}])/nElec;
-
-filter_array = reshape([source.avg.filter{:}], nElec, nVoxel)';
-virtChan_timeSeries = cellfun(@(x) filter_array*x, data.trial, 'UniformOutput', false);
-
-% convert the reconstructed time series into correct format
-virtChan_data = data;
-virtChan_data.label = cellstr(num2str(source.pos(source.inside,:)));
-virtChan_data.trial = virtChan_timeSeries;
-clear virtChan_timeSeries sourceData filter_array;
-
-% prealocate connectivity matrix
-connMatrix = zeros(nVoxel, nVoxel);
+cfg  = [];
+cfg.pos = source.pos(source.inside,:);
+virtChan_data = ft_virtualchannel(cfg,data,source);
+clear data source;
 
 % Frequency that is in the middle of the frequency band of interest
 halfrange = (params.freq_band.(freqBand)(2)-params.freq_band.(freqBand)(1))/2;
@@ -42,17 +39,19 @@ cfg.keeptrials = 'yes';
 cfg.pad = 'nextpow2';
 cfg.tapsmofrq = 1;
 virtFreq = ft_freqanalysis(cfg, virtChan_data);
+clear virtChan_data;
 
 % Connectivity
 cfg = [];
 cfg.method = 'wpli_debiased';
 source_conn = ft_connectivityanalysis(cfg, virtFreq);
-connMatrix(:,:) = source_conn.(['wpli_debiased','spctrm']);
-b = toc;
+clear virtFreq;
 
-[nRows, nCols] = size(connMatrix);
-connMatrix(1:nRows+1:nRows*nCols) = nan;
-disp([bidsID '_', freqBand, ' computation took ', num2str(b/60), ' minutes'])
-save(fullfile(params.connectivity_folder,[bidsID '_dwpli_' freqBand '.mat']),'connMatrix')
+% Average across frequency bins
+connMatrix = mean(abs(source_conn.wpli_debiasedspctrm),3);
 
+t = toc;
+disp([bidsID '_', freqBand, ' computation took ', num2str(t/60), ' minutes'])
+
+save(fullfile(params.connectivity_folder,[bidsID '_dwpli-Felix_' freqBand '.mat']),'connMatrix')
 end
