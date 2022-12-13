3eb3a49e418fdbcd1c29d514557c688e50aa777e
Cristina Gil <cristina.gil@tum.de>
2022-12-13 11:39:11 +0100

    deal with the case no peak found

--- custom_functions/plot_peakfrequency.m
+++ custom_functions/plot_peakfrequency.m
@@ -13,12 +13,15 @@ freqRange = find(power.freq >= params.freq_band.alpha(1) & power.freq <= params.
 pf_fig = figure('Position',[1988 548 781 781], 'visible', 'off');
 plot(power.freq(freqRange),avgpow(freqRange),'k');
 hold on
-h(1) = plot(peakfrequency.localmax,interp1(power.freq(freqRange),avgpow(freqRange),peakfrequency.localmax),'v','MarkerSize',8,'MarkerEdgeColor','#006633','MarkerFaceColor','#006633');
+plot(peakfrequency.localmax,interp1(power.freq(freqRange),avgpow(freqRange),peakfrequency.localmax),'v','MarkerSize',8,'MarkerEdgeColor','#006633','MarkerFaceColor','#006633');
 hold on
-h(2) = plot(peakfrequency.cog,interp1(power.freq(freqRange),avgpow(freqRange),peakfrequency.cog),'v','MarkerSize',8,'MarkerEdgeColor','#95C11F','MarkerFaceColor','#95C11F');
+plot(peakfrequency.cog,interp1(power.freq(freqRange),avgpow(freqRange),peakfrequency.cog),'v','MarkerSize',8,'MarkerEdgeColor','#95C11F','MarkerFaceColor','#95C11F');
 ylabel('Power (uV^2/Hz)')
 xlabel('Frequency (Hz)')
-legend(h,{'Maximum peak','Center of gravity'},'Location','southeast');
+% 'Fake' legend
+q(1) = plot(nan,'v','MarkerSize',8,'MarkerEdgeColor','#006633','MarkerFaceColor','#006633');
+q(2) = plot(nan,'v','MarkerSize',8,'MarkerEdgeColor','#95C11F','MarkerFaceColor','#95C11F');
+legend(q,{'Maximum peak','Center of gravity'},'Location','southeast');
 box off
 hold off
 
